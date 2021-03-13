8f06717e080a016bd4b23e378fd9eb2bc449b0e6
kcomas <kevin.comas.git@gmail.com>
2021-03-13 14:00:10 -0500

    render into page

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,13 @@
-let x: string = 'Hello World';
-console.log(x);
+import React from 'react';
+import { render } from 'react-dom';
+import { Button } from '@material-ui/core';
+
+function App() {
+    return <Button>Hello World</Button>;
+}
+
+const root = document.createElement('div');
+root.id = 'qi';
+document.body.prepend(root);
+
+render(<App />, root);
